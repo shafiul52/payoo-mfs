ba44fdea2f29fe36759ac95af5d972f71d2f5324
Your Name <shafiulhossain52@gmail.com>
2024-12-23 00:46:19 +0600

    added partially add money handler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,26 @@
-console.log('home added ')
+// add money to the account 
+/**
+ * s-1 add event hanler 
+  prevent page reload after form submit 
+  s -2  get money to be  added to the accout balance 
+ * 
+ * 
+ * 
+ */
+
+// stp1 add an event handler to the add money button inside the form 
+document.getElementById('btn-add-money')
+.addEventListener('click',function(event){
+    // prevent page reload after form submit
+    event.preventDefault();
+  
+    // s -2  get money to be  added to the accout
+        const addMoneyInput = document.getElementById('input-add-money').value;
+        console.log(addMoneyInput);
+
+        // get the pin number provided 
+
+        const  pinNumberInput = document.getElementById('input-pin-number').value;
+        console.log(pinNumberInput);
+
+})
