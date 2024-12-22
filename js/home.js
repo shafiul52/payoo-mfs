ea0d9786f647fe86c09e6194c7f51cfa98a3a080
Your Name <shafiulhossain52@gmail.com>
2024-12-23 01:28:38 +0600

    money added ar moddha new balance added

--- js/home.js
+++ js/home.js
@@ -2,7 +2,10 @@
 /**
  * s-1 add event hanler 
   prevent page reload after form submit 
-  s -2  get money to be  added to the accout balance 
+  *s -2  get money to be  added to the accout balance 
+   get the pin number 
+
+ *   s-3  verify the pin number 
  * 
  * 
  * 
@@ -21,6 +24,38 @@ document.getElementById('btn-add-money')
         // get the pin number provided 
 
         const  pinNumberInput = document.getElementById('input-pin-number').value;
-        console.log(pinNumberInput);
+        console.log(typeof pinNumberInput);
+
+    // stp 3 
+    // wrong way to pin number 
+    if(pinNumberInput=== '1234'){
+        console.log('adding money to your account ')
+
+        // stp4  get  the  current balance 
+        const balance  = document.getElementById('account-balance').innerText;
+        console.log(typeof balance)
+
+
+        // stp -5 add money input with balance 
+       const addMoneyNumber = parseFloat(addMoneyInput);
+       const balanceNumber = parseFloat(balance);
+        const newBalance  = addMoneyNumber + balanceNumber;
+        console.log(newBalance)
+
+
+         // stp 6 update  the  balance in the UI/dom 
+         document.getElementById('account-balance').innerText = newBalance;
+
+
+
+
+
+
+    }
+
+    else{
+        alert('failed to add money! Please try again.')
+      }
+
 
 })
