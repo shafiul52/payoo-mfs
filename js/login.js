394a8406b10ec094afc787b61c9ac45cf4ce4c61
Your Name <shafiulhossain52@gmail.com>
2024-12-22 23:36:44 +0600

    pin number added

--- js/login.js
+++ js/login.js
@@ -13,11 +13,23 @@ document.getElementById('button-login')
 .addEventListener('click',function(event){
     // stp2 prevent default behavior (reloading browser )
     event.preventDefault(); // ata aktu jotil lagba
- console.log('login button clicked');
 
 
-  // stp3  get the phone number 
+  // stp3  get the phone number  and get the pin number 
   const phoneNumber = document.getElementById('phone-number').value;
-  console.log(phoneNumber)
+  const pinNumber = document.getElementById('pin-number').value;
+  console.log(phoneNumber, pinNumber);
+
+
+  // stp 4 validate phone and pin 
+  // this is temporay way  you should not like this 
+  if(phoneNumber === '5'&& pinNumber === '1234'){
+        console.log('your are logged in ');
+        window.location.href = '/home.html'
+        // stp 5 allow user to the use the website 
+  }
+  else{
+    alert('wrong phone number or pin ')
+  }
 
 })
